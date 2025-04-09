5b80bf1cd88a07393da31aa0b8b1a66390c209c1
progrmador:dey <hola.120@upn.pe>
2025-04-08 19:17:08 -0500

    nuevo proyecto

--- proyecto1/Program.cs
+++ proyecto1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ namespace proyecto1
     {
         static void Main(string[] args)
         {
+            int num1, num2, num3, suma;
+            //Console.Readkey();
+            Console.Write("ingrese num1: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("ingrese num2: ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.Write("ingrese num3: ");
+            num3 = int.Parse(Console.ReadLine());
+            suma = num1 + num2 + num3;
+            Console.WriteLine("la suma es : " + suma);
         }
     }
 }
